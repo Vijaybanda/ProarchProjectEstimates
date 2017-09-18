564eea24449139ddf3792f283d6b5920ce23cd55
VijayB <vijayb@proarch.com>
2017-09-18 21:22:50 +0530

    Add project files.

---
+++ Proarch.Project.Estimates.DataAccess/DataAccessIocConfig.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proarch.Project.Estimates.DataAccess
+{
+    public static class DataAccessIocConfig
+    {
+        public static void RegisterDataAccessIoc(this IServiceCollection services)
+        {
+            
+        }
+
+    }
+}
---
+++ Proarch.Project.Estimates.Services/ServiceIocConfig.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proarch.Project.Estimates.Services
+{
+    public static class ServiceIocConfig
+    {
+        public static void RegisterServicesIoc(this IServiceCollection services)
+        {
+           
+        }
+    }
+}
